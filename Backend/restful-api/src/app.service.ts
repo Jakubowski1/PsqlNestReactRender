03a48c1c11ccc8e5af37a0218afa2a7611c728be
Mati <jakubowski.projects@gmail.com>
2024-08-21 23:36:06 +0200

    feat: migration

--- Backend/restful-api/src/app.service.ts
+++ Backend/restful-api/src/app.service.ts
@@ -1,7 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
+import { UserService } from './user/user.service';
+import { User } from './user/user.entity';
+import { Role } from './constants/roles.enum';
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  constructor(
+
+    private readonly usersService: UserService,
+  ) {}
+
+  async onModuleInit() {
+    const user = new User();
+    user.surname = 'admin';
+    user.name = 'admin'
+    user.email = 'admin'
+    user.password = 'admin'; 
+    user.role = Role.Manager;
+   
+    const createdUser = await this.usersService.create(user);
+
+
+
+  }
+  
+
   getHello(): string {
     return 'Hello World!';
   }
---
+++ Backend/restful-api/src/src/migrations/1724276111752-MigrationForNullEntries.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MigrationForNullEntries1724276111752 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
