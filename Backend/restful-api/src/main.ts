2e505efeae5521ae5f47bcaa729f954b4f1cdc24
Mati <jakubowski.projects@gmail.com>
2024-09-02 19:32:39 +0200

    feat:added CORS

--- Backend/restful-api/src/main.ts
+++ Backend/restful-api/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000', // Your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Allows cookies or authorization headers to be sent with requests
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('no words that can describe it')
